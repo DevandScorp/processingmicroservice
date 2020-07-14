9d28e99731ce9e552fb22682cafb4a389068c68b
DevandScorp <artemda459@gmail.com>
2020-07-14 23:04:11 +0300

    feat: changed default user picture

--- service/telegram.service.js
+++ service/telegram.service.js
@@ -121,7 +121,7 @@ class AppMonitoringService {
         first_name: user.first_name || '',
         last_name: user.last_name || '',
         username: user.username || '',
-        image_url: image_url || 'https://capenetworks.com/static/images/testimonials/user-icon.svg',
+        image_url: image_url || 'https://test.developmentandscorp.tech/user.svg',
       };
     });
     chats = chats.map(async (chat) => {
